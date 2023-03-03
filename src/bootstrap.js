d6e24586e87cc5d46ea7b27b0e99cd148dc72ca5
deosidd <Siddarth.deo@carrier.com>
2023-03-03 12:55:18 +0530

    Added mount function

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,10 +1,22 @@
-import faker from 'faker';
+import faker from "faker";
 
-let products = '';
+const mount = (el) => {
+  let products = "";
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+  products = `<h4>Welcome to Prodcut MFE</h4> ${products}`;
+  el.innerHTML = products;
+};
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-products");
+  /** Assuming dev-products element is not included in container app */
+  if (el) {
+    mount(el);
+  }
 }
-products = `<h4>Welcome to Prodcut MFE</h4> ${products}`
-document.querySelector("#dev-products").innerHTML = products;
+
+export { mount };
